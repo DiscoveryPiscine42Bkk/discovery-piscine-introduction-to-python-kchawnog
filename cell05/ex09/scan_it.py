4c8cce7768543f4791d6bfc95b09b293a9a9c691
kchawnog <kanokwanchawong@gmail.com>
2025-06-19 06:44:39 +0000

    ex09

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -4,7 +4,6 @@ import sys
 def main():
     if len(sys.argv)!=3:
         print("none")
-        return
 
 keyword = sys.argv[1]
 text = sys.argv[2]
@@ -14,5 +13,4 @@ if count == 0:
     print("none")
 else:
     print(count)
-    main()
     
